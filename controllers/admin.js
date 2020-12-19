7a818420b0db6e66e01fda58bacb4c174e1b1553
thang-rikai <thang.nguyen@rikai.technology>
2020-12-19 11:22:13 +0700

    advance authorization & send email

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,11 @@
 const Product = require("../models/product");
+const mongoose = require("mongoose");
 
 exports.getAddProduct = (req, res, next) => {
 	res.render("admin/edit-product", {
 		pageTitle: "Add Product",
 		path: "/admin/add-product",
 		editing: false,
-		isAuthenticated: req.session.isLoggedIn,
 	});
 };
 
@@ -24,7 +24,6 @@ exports.postAddProduct = (req, res, next) => {
 	product
 		.save()
 		.then((result) => {
-			// console.log(result);
 			console.log("Created Product");
 			res.redirect("/admin/products");
 		})
@@ -49,7 +48,6 @@ exports.getEditProduct = (req, res, next) => {
 				path: "/admin/edit-product",
 				editing: editMode,
 				product: product,
-				isAuthenticated: req.session.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
@@ -61,24 +59,31 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedPrice = req.body.price;
 	const updatedImageUrl = req.body.imageUrl;
 	const updatedDesc = req.body.description;
-
 	Product.findById(prodId)
 		.then((product) => {
+			console.log("edit product: ", product);
+			console.log("compare user id: ", product.userId !== req.user.id);
+
+			if (product.userId.toString() !== req.user._id.toString()) {
+				return res.redirect("/");
+			}
+
 			product.title = updatedTitle;
 			product.price = updatedPrice;
 			product.description = updatedDesc;
 			product.imageUrl = updatedImageUrl;
-			return product.save();
+
+			return product.save().then((result) => {
+				console.log("UPDATED PRODUCT!");
+				res.redirect("/admin/products");
+			});
 		})
-		.then((result) => {
-			console.log("UPDATED PRODUCT!");
-			res.redirect("/admin/products");
-		})
+
 		.catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-	Product.find()
+	Product.find({ userId: req.user._id })
 		// .select('title price -_id')
 		// .populate('userId', 'name')
 		.then((products) => {
@@ -86,7 +91,6 @@ exports.getProducts = (req, res, next) => {
 				prods: products,
 				pageTitle: "Admin Products",
 				path: "/admin/products",
-				isAuthenticated: req.session.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
@@ -94,7 +98,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.findByIdAndRemove(prodId)
+	Product.deleteOne({ _id: prodId, userId: req.user._id })
 		.then(() => {
 			console.log("DESTROYED PRODUCT");
 			res.redirect("/admin/products");
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,43 @@
+const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+
+const transporter = nodemailer.createTransport(
+	sendgridTransport({
+		auth: {
+			api_key:
+				"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",
+		},
+	})
+);
 
 exports.getLogin = (req, res, next) => {
+	let message = req.flash("error");
+	if (message.length > 0) {
+		message = message[0];
+	} else {
+		message = null;
+	}
 	res.render("auth/login", {
 		path: "/login",
 		pageTitle: "Login",
-		isAuthenticated: false,
+		errorMessage: message,
 	});
 };
 
 exports.getSignup = (req, res, next) => {
+	let message = req.flash("error");
+	if (message.length > 0) {
+		message = message[0];
+	} else {
+		message = null;
+	}
 	res.render("auth/signup", {
 		path: "/signup",
 		pageTitle: "Signup",
-		isAuthenticated: false,
+		errorMessage: message,
 	});
 };
 
@@ -23,6 +47,7 @@ exports.postLogin = (req, res, next) => {
 	User.findOne({ email: email })
 		.then((user) => {
 			if (!user) {
+				req.flash("error", "Invalid email or password");
 				return res.redirect("/login");
 			}
 			bcrypt
@@ -53,6 +78,11 @@ exports.postSignup = (req, res, next) => {
 	User.findOne({ email: email })
 		.then((userDoc) => {
 			if (userDoc) {
+				req.flash(
+					"error",
+					"E-Mail exists already, please pick a different one."
+				);
+
 				return res.redirect("/signup");
 			}
 			return bcrypt
@@ -67,6 +97,17 @@ exports.postSignup = (req, res, next) => {
 				})
 				.then((result) => {
 					res.redirect("/login");
+					console.log("Email: ", email);
+					var mailOptions = {
+						to: email,
+						from: "thang.nguyen@rikai.technology",
+						subject: "Signup succeeded!",
+						html: "<h1>You successfully signed up!</h1>",
+					};
+					return transporter.sendMail(mailOptions);
+				})
+				.catch((err) => {
+					console.log(err);
 				});
 		})
 		.catch((err) => {
@@ -78,4 +119,106 @@ exports.postLogout = (req, res, next) => {
 	req.session.destroy((err) => {
 		res.redirect("/");
 	});
+};
+
+exports.getReset = (req, res, next) => {
+	let message = req.flash("error");
+	if (message.length > 0) {
+		message = message[0];
+	} else {
+		message = null;
+	}
+	res.render("auth/reset", {
+		path: "/reset",
+		pageTitle: "Reset Password",
+		errorMessage: message,
+	});
+};
+
+exports.postReset = (req, res, next) => {
+	crypto.randomBytes(32, (err, buffer) => {
+		if (err) {
+			console.log(err);
+			return res.redirect("/reset");
+		}
+		const token = buffer.toString("hex");
+		User.findOne({ email: req.body.email })
+			.then((user) => {
+				if (!user) {
+					req.flash("error", "No account with that email found.");
+					return res.redirect("/reset");
+				}
+				user.resetToken = token;
+				user.resetTokenExpiration = Date.now() + 3600000;
+				return user.save();
+			})
+			.then((result) => {
+				res.redirect("/");
+				transporter.sendMail({
+					to: req.body.email,
+					from: "thang.nguyen@rikai.technology",
+					subject: "Password reset",
+					html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+          `,
+				});
+				console.log("receive Email:", req.body.email);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
+};
+
+exports.getNewPassword = (req, res, next) => {
+	const token = req.params.token;
+	User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+		.then((user) => {
+			let message = req.flash("error");
+			if (message.length > 0) {
+				message = message[0];
+			} else {
+				message = null;
+			}
+			res.render("auth/new-password", {
+				path: "/new-password",
+				pageTitle: "New Password",
+				errorMessage: message,
+				userId: user._id.toString(),
+				passwordToken: token,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+exports.postNewPassword = (req, res, next) => {
+	const newPassword = req.body.password;
+	const userId = req.body.userId;
+	const passwordToken = req.body.passwordToken;
+	let resetUser;
+
+	User.findOne({
+		resetToken: passwordToken,
+		resetTokenExpiration: { $gt: Date.now() },
+		_id: userId,
+	})
+		.then((user) => {
+			resetUser = user;
+			return bcrypt.hash(newPassword, 12);
+		})
+		.then((hashedPassword) => {
+			resetUser.password = hashedPassword;
+			resetUser.resetToken = undefined;
+			resetUser.resetTokenExpiration = undefined;
+			return resetUser.save();
+		})
+		.then((result) => {
+			res.redirect("/login");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
